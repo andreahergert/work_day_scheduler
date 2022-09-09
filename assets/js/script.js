26b6152917a888e6e35692947a9972e25aa8a2f9
Andrea Hergert <ahergert24@hotmail.com>
2022-09-09 15:50:07 -0700

    added the day of the week, time, and date using moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
 // javascript goes here
+// Need current day to display at top of calendar
+// Colorcoded Timeblocks
+    // past
+    // present
+    // future
+// Enter an event
+// save button saves to local storage
+// saved events persist
+
+var todayDate = moment().format('dddd, MMMM Do');
+$("#currentDay").html(todayDate);
