2852e3b166234ae8806967fc463355ecd3f6b92a
Andrea Hergert <ahergert24@hotmail.com>
2022-09-10 16:52:10 -0700

    added color in description blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,18 @@
 var todayDate = moment().format('dddd, MMMM Do');
 $("#currentDay").html(todayDate);
 
+function timeBlockColor() {
+    var hour = moment().hours();
 
+    $(".time-block").each(function() {
+        var timeNow = parseInt($(this).attr("id"));
+        if (timeNow > hour) {
+            $(this).addClass("future");
+        } else if (timeNow === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+timeBlockColor();
