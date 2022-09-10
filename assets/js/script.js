cf56283a9cd88799da35ffde74fa4e13c6e50fad
Andrea Hergert <ahergert24@hotmail.com>
2022-09-09 22:44:10 -0700

    need to redo description colors, starting over with that

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,29 +11,4 @@
 var todayDate = moment().format('dddd, MMMM Do');
 $("#currentDay").html(todayDate);
 
-// Tracks time from moment making the display description background change color based on time
-    function timeTracker() {
-        //get current number of hours.
-        var currentTime = moment().hour();
 
-        $(".time-block").each(function () {
-            var timeBlock = parseInt($(this).attr("time-block").split("hour")[1]);
-
-            if (timeBlock < currentTime) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-            }
-            else if (timeBlock === currentTime) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-
-            }
-        })
-    }
